61572f0ac5802e3450ca10e93651cd521c38891b
이성훈 <dgh0612321@naver.com>
2026-02-14 19:41:42 +0900

    first commit

---
+++ BE/src/test/java/com/springboot/smartflow/SmartFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.smartflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
